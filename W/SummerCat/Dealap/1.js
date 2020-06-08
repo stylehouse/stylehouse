4e7159ac172f91b5573b500d11c762d2076a665c
Steve <ostylehouse@gmail.com>
2020-06-08 01:17:32 -0700

    washad

--- W/SummerCat/Dealap/1.js
+++ W/SummerCat/Dealap/1.js
@@ -1,20 +1,20 @@
-function Dealap_current () { return Dealap_bb08fc85f8b1() }
-function Dealap_bb08fc85f8b1 () { return [
-["","J","001",{"W":"of"},{"icountre":8}],
-  ["  ","J","001",{"of":"J"},{"of":"J","strinoge":4,"dige":"ce3edaeb5086","digs":"58624b5aef06","name":"Dealap_bb08fc85f8b1_J_001_fleow_2","lines":4,"line":8}],
-["","i","001",{"W":"of"},{"icountre":8}],
-  ["  ","i","001",{"of":"i"},{"of":"i","strinoge":7,"dige":"f7f18aae3a47","digs":"ae7d1277c2c4","name":"Dealap_bb08fc85f8b1_i_001_fal_1","lines":7,"line":12}],
-  ["  ","i","001",{"of":"i"},{"of":"i","strinoge":4,"dige":"855dd8500f3c","digs":"dbe8a0cfb231","name":"Dealap_bb08fc85f8b1_i_001_zeal_1","lines":4,"line":19}],
+function Dealap_current () { return Dealap_e6bd003723b8() }
+function Dealap_e6bd003723b8 () { return [
+["","J","001",{"W":"of"},{}],
+  ["  ","J","001",{"of":"J"},{"of":"J","strinoge":4,"dige":"ce3edaeb5086","digs":"58624b5aef06","name":"Dealap_e6bd003723b8_J_001_fleow_2","lines":4,"line":8}],
+["","i","001",{"W":"of"},{}],
+  ["  ","i","001",{"of":"i"},{"of":"i","strinoge":7,"dige":"f7f18aae3a47","digs":"ae7d1277c2c4","name":"Dealap_e6bd003723b8_i_001_fal_1","lines":7,"line":12}],
+  ["  ","i","001",{"of":"i"},{"of":"i","strinoge":4,"dige":"049f32a88a34","digs":"4007de290389","name":"Dealap_e6bd003723b8_i_001_zeal_1","lines":4,"line":19}],
 ]}
-function Dealap_bb08fc85f8b1_J_001_fleow_2 (A,C,G,T,s) {
+function Dealap_e6bd003723b8_J_001_fleow_2 (A,C,G,T,s) {
 G.h(A,C,G,T,'laterings')
 }
-function Dealap_bb08fc85f8b1_i_001_fal_1 (A,C,G,T,s) {
+function Dealap_e6bd003723b8_i_001_fal_1 (A,C,G,T,s) {
 if (hype) {
     me['ha'](A,C,G,T,'more')
 }
 return 3 * 3
 }
-function Dealap_bb08fc85f8b1_i_001_zeal_1 (A,C,G,T,s) {
-G.h(A,C,G,T,'figurinia')
+function Dealap_e6bd003723b8_i_001_zeal_1 (A,C,G,T,s) {
+G.h(A,C,G,T,'figuriniarrto')
 }
