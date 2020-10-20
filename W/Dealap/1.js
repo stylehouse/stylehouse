df1299a363f803712579c6561731f626776e3de9
Steve <ostylehouse@gmail.com>
2020-10-19 23:38:15 -0700

    w:Fish, @Jrunto &t u

--- W/Dealap/1.js
+++ W/Dealap/1.js
@@ -1,20 +1,24 @@
-function Dealap_current () { return Dealap_cfcef0de34ae() }
-function Dealap_cfcef0de34ae () { return [
+function Dealap_current () { return Dealap_2665815a3a64() }
+function Dealap_2665815a3a64 () { return [
 ["","J","001",{"W":"of"},{}],
-  ["  ","J","001",{"of":"J"},{"of":"J","dige":"ce3edaeb5086","strinoge":2,"digs":"58624b5aef06","name":"Dealap_cfcef0de34ae_J_001_fleow_2","lines":4,"line":8}],
+  ["  ","J","001",{"of":"J"},{"of":"J","dige":"ce3edaeb5086","strinoge":2,"digs":"58624b5aef06","name":"Dealap_2665815a3a64_J_001_fleow_2","lines":4,"line":8}],
 ["","i","001",{"W":"of"},{}],
-  ["  ","i","001",{"of":"i"},{"of":"i","dige":"f7f18aae3a47","strinoge":3,"digs":"ae7d1277c2c4","name":"Dealap_cfcef0de34ae_i_001_fal_1","lines":7,"line":12}],
-  ["  ","i","001",{"of":"i"},{"of":"i","dige":"1ca1eeb64ec2","strinoge":2,"digs":"986ea744d0c6","name":"Dealap_cfcef0de34ae_i_001_zeal_1","lines":4,"line":19}],
+  ["  ","i","001",{"of":"i"},{"of":"i","dige":"57f1e9f8655b","strinoge":7,"digs":"4a3d3c9f7fdd","name":"Dealap_2665815a3a64_i_001_fal_1","lines":11,"line":12}],
+  ["  ","i","001",{"of":"i"},{"of":"i","dige":"1ca1eeb64ec2","strinoge":2,"digs":"986ea744d0c6","name":"Dealap_2665815a3a64_i_001_zeal_1","lines":4,"line":23}],
 ]}
-function Dealap_cfcef0de34ae_J_001_fleow_2 (A,C,G,T,s) {
+function Dealap_2665815a3a64_J_001_fleow_2 (A,C,G,T,s) {
 G.h(A,C,G,T,'laterings')
 }
-function Dealap_cfcef0de34ae_i_001_fal_1 (A,C,G,T,s) {
+function Dealap_2665815a3a64_i_001_fal_1 (A,C,G,T,s) {
 if (hype) {
     me['ha'](A,C,G,T,'more')
 }
+var BlockQuote =
+"&figures\n";
+G.h(A,C,G,T,"n",["leaving","3",{s:BlockQuote},{}]);
+&skating,round
 return 3 * 3
 }
-function Dealap_cfcef0de34ae_i_001_zeal_1 (A,C,G,T,s) {
+function Dealap_2665815a3a64_i_001_zeal_1 (A,C,G,T,s) {
 G.h(A,C,G,T,'figuriniaroroto')
 }
